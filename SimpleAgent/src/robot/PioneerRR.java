4e01ba0e9e27a72d296f5b6368ae4b4999e953c9
Sebastian Rockel <sebastianrockel@gmail.com>
2011-01-04 12:25:04 +0100

    fix hardware IF, add robot class

---
+++ SimpleAgent/src/robot/PioneerRR.java
@@ -0,0 +1,42 @@
+package robot;
+
+import data.Position;
+import device.Ranger;
+
+import javaclient3.PlayerException;
+
+final public class PioneerRR extends Pioneer {
+
+	public PioneerRR(String name, int port, int id) {
+		super(name, port, id);
+		try {
+			this.laser    = new Ranger (this.playerclient, super.id, 1);
+			this.sonar 	  = new Ranger (this.playerclient, super.id, 0);
+		} catch (PlayerException e) {
+			System.err.println ("PioneerSL: > Error connecting to Player: ");
+			System.err.println ("    [ " + e.toString() + " ]");
+			System.exit (1);
+		}
+		this.playerclient.runThreaded (-1, -1);
+	}
+
+	@Override
+	public void shutdownDevices() {
+		this.laser.thread.interrupt();
+		while(this.laser.thread.isAlive());
+		this.sonar.thread.interrupt();
+		while(this.sonar.thread.isAlive());
+	}
+
+	@Override
+	public void setGoal(Position goal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Position getGoal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ SimpleAgent/src/usecase/Wallfollow.java
@@ -0,0 +1,57 @@
+/// @file wallfollowing.cpp
+/// @author Sebastian Rockel
+///
+/// @mainpage Robotic Project 2009
+///
+/// @par Copyright
+/// Copyright (C) 2009 Sebastian Rockel.
+/// This program can be distributed and modified under the condition mentioning
+/// the @ref author.
+///
+/// @par Description
+/// Wall following example for Pioneer 2DX robot.
+/// This is part of the robotics students project at Uni Hamburg in 2009.
+/// The task was to implement sensor fusion (besides laser and sonar also
+/// omnidirectional camera) to find and track a (pink) ball or when idle do
+/// exploring floor and rooms (via wall following) always avoiding collisions
+/// with static or dynamic obstacles.
+///
+/// @par Sensor fusion
+/// The program depends on sensor fusion of 16 sonar rangers and a 240 degree
+/// urg laser ranger (as well as an omni directional camera when enabled, see
+/// makefile).
+///
+/// @par Source code
+/// @code git clone git://github.com/buzzer/Pioneer-Project-2009.git @endcode
+///
+/// (Scalable) Vector Graphic formatted figures :
+/// @image html PioneerShape.svg "Overview about how laser and sonar sensors are fused"
+/// @image html AngleDefinition.svg "Calculating turning angle via the atan function"
+/// @image latex PioneerShape.pdf "Overview about how laser and sonar sensors are fused" width=\textwidth
+/// @image latex AngleDefinition.pdf "Calculating turning angle via the atan function" width=0.8\textwidth
+/// Same graphics in Portable Network Graphic format (html only):
+/// @image html PioneerShape.png "Overview about how laser and sonar sensors are fused"
+/// @image html AngleDefinition.png "Calculating turning angle via the atan function"
+package usecase;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import robot.PioneerRR;
+
+public class Wallfollow {
+
+	private static BufferedReader in = new BufferedReader(
+          new InputStreamReader(System.in));
+
+	public static void main (String[] args) {
+		try {
+			PioneerRR pionRR = new PioneerRR("localhost", 6665, 1);
+			
+			// Wait until enter is pressed
+			in.readLine();
+			pionRR.shutdown();
+			
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+}
