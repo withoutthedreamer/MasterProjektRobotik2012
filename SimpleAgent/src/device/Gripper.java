e74fbf3a454823032467afc22141317c20e03112
Sebastian Rockel <sebastianrockel@gmail.com>
2011-02-22 14:31:25 +0100

    fixed Gripper lift

--- SimpleAgent/src/device/Gripper.java
+++ SimpleAgent/src/device/Gripper.java
@@ -8,6 +8,11 @@ public class Gripper extends RobotDevice
 {
     /** Logging support */
     Logger logger = Logger.getLogger (Gripper.class.getName ());
+    
+    /** Controls a lift on the gripper (if gripper equipped) */
+    Actarray aa;
+    /** Returns data from digital I/O ports (if equipped) */
+    Dio dio;
 
 	/**
 	 * Taken from the player IF documantation.
@@ -16,6 +21,8 @@ public class Gripper extends RobotDevice
 		OPEN,
 		CLOSED,
 		MOVING,
+		UP,
+		DOWN,
 		ERROR
 	}
 
@@ -29,9 +36,16 @@ public class Gripper extends RobotDevice
 		((GripperInterface) device).stop();
 	}
 	public void open () {
+		updateDio();
+		
 		((GripperInterface) device).open();
-
-		try { Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
+		
+		if (dio != null) {
+			while (dio.getInput(0) != 0) {
+				try { Thread.sleep(100); } catch (InterruptedException e) { e.printStackTrace(); }
+			}
+		} else
+			try { Thread.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
 	}
 	public void close () {
 		((GripperInterface) device).close();
@@ -40,57 +54,68 @@ public class Gripper extends RobotDevice
 	}
 	public void lift ()
 	{
-	    Dio dio = getDio();
-	    
-	    if (dio != null)
+		updateActarray();
+		updateDio();
+
+		if (dio != null)
 	    	/** Some thing between the paddles ? */
-	        if (dio.getInput(3)==1 || dio.getInput(2)==1 )
-	            logger.info("Something is between the paddles");
-	        else
+	    	if (dio.getInput(3)==1 || dio.getInput(2)==1 ) {
+	    		logger.info("Something is between the paddles");
+	    	} else
 	            logger.info("Nothing between the paddles");
-	    
-	    /** Close paddles */
-	    close();
-	    
-	    Actarray aa = getActarray();
-	    if (aa != null)
+
+	    if (aa != null) {
+	    	/** Lift up */
 	        aa.moveHome(0);
-	    else
-            logger.config("Actarray not found");
+	    }
+	   	   
+//		if (dio != null) {
+//			while (dio.getInput(1) != 0) {
+//				try { Thread.sleep(100); } catch (InterruptedException e) { e.printStackTrace(); }
+//			}
+//		} else
+			try { Thread.sleep(4000); } catch (InterruptedException e) { e.printStackTrace(); }
 	}
 	
 	public void release ()
 	{
-	    Actarray aa = getActarray();
-	    
-	    if (aa != null)
-	        aa.moveTo(0, 0);
-	    else
-            logger.config("Actarray not found");
-	    
-	    /** Open paddles */
-	    open();
-	}
-	
-	Actarray getActarray()
-	{
-	    Actarray aa = (Actarray) getDeviceNode().getDevice(new Device(IDevice.DEVICE_ACTARRAY_CODE, null,-1,-1));
+		updateActarray();
+		updateDio();
 
-	    if (aa == null) {
-	        logger.info("No Actarray device found");
+		/** Lift down */
+	    if (aa != null) {
+	        aa.moveTo(0, 0);
 	    }
+	   
+	    if (dio != null) {
+	    	while (dio.getInput(1) != 0) {
+	    		try { Thread.sleep(100); } catch (InterruptedException e) { e.printStackTrace(); }
+	    	}
+	    } else
+	    	try { Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
+    }
 	
-	    return aa;
+	void updateActarray()
+	{				
+		if (aa == null) {
+			aa = (Actarray) getDeviceNode().getDevice(new Device(IDevice.DEVICE_ACTARRAY_CODE, null,-1,-1));
+
+			if (aa == null) {
+				logger.info("No Actarray device found");
+				aa = new Actarray();
+			}
+		}
 	}
-	Dio getDio()
+	void updateDio()
 	{
-	    Dio dio = (Dio) getDeviceNode().getDevice(new Device(IDevice.DEVICE_DIO_CODE,null,-1,-1));
-	    
-        if (dio == null) {
-            logger.info("No Dio device found");
-        }
-        
-        return dio;
+		if (dio == null) {
+			dio = (Dio) getDeviceNode().getDevice(new Device(IDevice.DEVICE_DIO_CODE,null,-1,-1));
+
+			if (dio == null) {
+				logger.info("No Dio device found");
+				dio = new Dio();
+			}
+		}
 	}
 
 	public stateType getState() {
