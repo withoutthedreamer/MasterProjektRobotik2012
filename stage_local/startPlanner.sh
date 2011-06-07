c0b6a2bd61e9d87d1646325cb5806d403a74fdc2
Sebastian Rockel <sebastianrockel@gmail.com>
2011-06-07 09:18:56 +0200

    added copyright, small cleaning

--- stage_local/startPlanner.sh
+++ stage_local/startPlanner.sh
@@ -3,12 +3,10 @@
 # Start Player planner servers
 # Multiple robots scenario
 
-cd ~/robotcolla/SimpleAgent/player/
-
 player -p 6666 planner_6666.cfg &
 player -p 6668 planner_6668.cfg &
 player -p 6670 planner_6670.cfg &
 
-sleep 1
+sleep 2
 
 playernav localhost:6665 localhost:6666 localhost:6668 localhost:6670 &
--- stage_local/startSimulation.sh
+++ stage_local/startSimulation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 2011-03-30 Sebastian Rockel
+# Starts Stage simulation with robots
 
 killall player
 killall player
@@ -7,8 +8,9 @@ killall player
 killall playernav
 
 
-player ~/robotcolla/SimpleAgent/player/uhh1.cfg &
-player -p 6600 ~/robotcolla/SimpleAgent/player/uhhsimu1.cfg &
-sleep 2
-cd ~/robotcolla/SimpleAgent/player/
+player uhh1.cfg &
+#player -p 6600 uhhsimu1.cfg &
+
+sleep 3
+
 ./startPlanner.sh &
