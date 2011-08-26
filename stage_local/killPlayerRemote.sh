bffadacc3123fb69921b63840bdf6c6e53379488
Sebastian Rockel <sebastianrockel@gmail.com>
2011-08-26 15:41:30 +0200

    fixed findNCollect scenario

--- stage_local/killPlayerRemote.sh
+++ stage_local/killPlayerRemote.sh
@@ -7,6 +7,11 @@ host3="134.100.13.163"
 locDir="/tmp/simpleagent/player"
 user="demo"
 
-ssh -f $user@$host1 "killall player; killall player; killall player"
-ssh -f $user@$host3 "killall player; killall player; killall player"
-ssh -f $user@$host2 "killall player; killall player; killall player"
+if [ -z "$1" ] ; then
+  ssh -f $user@$host1 "killall player; killall player; killall player"
+  ssh -f $user@$host3 "killall player; killall player; killall player"
+  ssh -f $user@$host2 "killall player; killall player; killall player"
+else
+  host="$1"
+  ssh -f $user@$host "killall player; killall player; killall player"
+fi
--- stage_local/startPlayerRemote.sh
+++ stage_local/startPlayerRemote.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-host1="tams66"
-host2="tams67"
-host3="134.100.13.163"
-
 locDir="/tmp/simpleagent/player"
 user="demo"
 
-ssh -f $user@$host1 "cd $locDir; $locDir/startPlayer.sh"
-ssh -f $user@$host3 "cd $locDir; $locDir/startPlayer.sh"
-ssh -f $user@$host2 "cd $locDir; $locDir/startPlayer.sh"
+source killPlayerRemote.sh "$1"
+
+if [ -z $1 ] ; then
+  host1="tams66"
+  host2="tams67"
+  host3="134.100.13.163"
+
+  ssh -f $user@$host1 "cd $locDir; $locDir/startPlayer.sh"
+  ssh -f $user@$host3 "cd $locDir; $locDir/startPlayer.sh"
+  ssh -f $user@$host2 "cd $locDir; $locDir/startPlayer.sh"
+else
+  host="$1"
+  ssh -f $user@$host "cd $locDir; $locDir/startPlayer.sh"
+  sleep 5
+  playernav localhost:6665 localhost:6672 &
+  ssh -F "/Users/sebastian/.ssh/tams" $host"_L6671"
+fi
+
+
