b50fe5d778ba0588c62d6ecbe00b785960883fb2
Sebastian Rockel <rockel@informatik.uni-hamburg.de>
2011-10-06 12:08:34 +0200

    added start script for uhh world

---
+++ stage_local/startTest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 2011-03-30 Sebastian Rockel
+# Starts Stage simulation with robots
+
+killall player
+killall player
+killall player
+killall playernav
+
+
+player uhh.cfg &
+#player -p 6600 uhhsimu1.cfg &
+
+sleep 3
+
+./startPlanner.sh &
