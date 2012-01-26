e03c5908d32b899ff41a295255c41288c6640e52
Christoph Koch <6koch@informatik.uni-hamburg.de>
2012-01-26 17:17:27 +0100

    Formatierungskram

--- SimpleAgent/src/robot/PatrolRobot.java
+++ SimpleAgent/src/robot/PatrolRobot.java
@@ -10,12 +10,17 @@ import de.unihamburg.informatik.tams.project.communication.exploration.Explorati
 import device.Device;
 import device.Planner;
 import device.external.IGripperListener;
+import device.external.IPlannerListener;
 
 public abstract class PatrolRobot extends NavRobot {
 
 	protected MapPosition position;
 
 	protected RobotState state;
+
+	protected RobotState plannerState;
+
+	protected RobotState gripperState;
 
 	protected Planner planner;
 	
@@ -35,14 +40,15 @@ public abstract class PatrolRobot extends NavRobot {
 
 	public State getState() {
 		State result = null;
-		if(state == RobotState.NEEDS_NEW_GOAL || state == RobotState.ON_THE_WAY) {
+		if (state == RobotState.NEEDS_NEW_GOAL
+				|| state == RobotState.ON_THE_WAY) {
 			result = State.EXPLORING;
-		} else if(state == RobotState.TRANSPORTING_BARREL) {
+		} else if (state == RobotState.TRANSPORTING_BARREL) {
 			result = State.TRANSPORTING;
 		}
 		return result;
 	}
-	
+
 	public void setState(RobotState state) {
 		this.state = state;
 	}
@@ -51,19 +57,141 @@ public abstract class PatrolRobot extends NavRobot {
 		return position;
 	}
 
-	public void transportBarrelTo(MapPosition currentPositionOfBarrel, MapPosition targetPositionOfBarrel) {
-		Position barrelPos = new Position(currentPositionOfBarrel.getxPosition(), currentPositionOfBarrel.getyPosition(), 0);
-		IGripperListener gl = new IGripperListener() {
-			public void whenOpened(){}
-			public void whenClosed(){}
-			public void whenLifted(){}
-			public void whenReleased(){}
-			public void whenClosedLifted(){}
-			public void whenReleasedOpened(){}
-			public void whenError(){}
+	public void transportBarrelTo(MapPosition currentPositionOfBarrel,
+			MapPosition targetPositionOfBarrel) {
+			// state setzen
+			state = RobotState.TRANSPORTING_BARREL;
+
+			// Position der Barrel und des Ziels erzeugen
+			Position barrelPos = new Position(
+				currentPositionOfBarrel.getxPosition(),
+				currentPositionOfBarrel.getyPosition(), 0);
+			Position targetPos = new Position(
+				targetPositionOfBarrel.getxPosition(),
+				targetPositionOfBarrel.getyPosition(), 0);
+
+			// IGripperListener für den Gripper
+			IGripperListener gl = new IGripperListener() {
+			public void whenOpened() {
+				gripperState = RobotState.GRIPPER_OPEN;
+				gripper.removeIsDoneListener(this);
+			}
+			public void whenClosed() {}
+			public void whenLifted() {}
+			public void whenReleased() {}
+			public void whenClosedLifted() {
+				gripperState = RobotState.GRIPPER_CLOSE;
+				gripper.removeIsDoneListener(this);
+			}
+			public void whenReleasedOpened() {
+				gripperState = RobotState.GRIPPER_OPEN;
+				gripper.removeIsDoneListener(this);
+			}
+			public void whenError() {}
 		};
+		// Gripper öffnen
+		gripperState = RobotState.OPENING_GRIPPER;
 		getGripper().open(gl);
-		getPlanner().setGoal(barrelPos);
+		// Warten bis Gripper offen
+		while (gripperState != RobotState.GRIPPER_OPEN) {
+			try {
+				Thread.sleep(500); // 500ms warten bis gripperState erneut
+									// abgefragt wird
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Zur Barrel fahren
+		plannerState = RobotState.DRIVING_TO_BARREL;
+		planner.addIsDoneListener(new IPlannerListener() {
+			@Override
+			public void callWhenIsDone() {
+				plannerState = RobotState.BARREL_REACHED;
+			}
+			@Override
+			public void callWhenAbort() {}
+			@Override
+			public void callWhenNotValid() {}
+		});
+		planner.setGoal(barrelPos);
+		// Warten bis Barrel erreicht wurde
+		while (plannerState != RobotState.BARREL_REACHED) {
+			try {
+				Thread.sleep(500); // 500ms warten bis plannerState erneut
+									// abgefragt wird
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Gripper schliessen
+		gripperState = RobotState.CLOSING_GRIPPER;
+		getGripper().closeLift(gl);
+		// Warten bis Gripper geschlossen
+		while (gripperState != RobotState.GRIPPER_CLOSE) {
+			try {
+				Thread.sleep(500); // 500ms warten bis gripperState erneut
+									// abgefragt wird
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Barrel zum Ziel fahren
+		plannerState = RobotState.TRANSPORTING_BARREL;
+		planner.addIsDoneListener(new IPlannerListener() {
+			@Override
+			public void callWhenIsDone() {
+				plannerState = RobotState.BARREL_TARGET_REACHED;
+			}
+			@Override
+			public void callWhenAbort() {}
+			@Override
+			public void callWhenNotValid() {
+				logger.info("No valid path");
+			}
+		});
+		planner.setGoal(targetPos);
+		// Warten bis Ziel erreicht wurde
+		while (plannerState != RobotState.BARREL_TARGET_REACHED) {
+			try {
+				Thread.sleep(500); // 500ms warten bis plannerState erneut
+									// abgefragt wird
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Barrel absetzen
+		gripperState = RobotState.OPENING_GRIPPER;
+		getGripper().releaseOpen(gl);
+		// Warten bis Gripper offen
+		while (gripperState != RobotState.GRIPPER_OPEN) {
+			try {
+				Thread.sleep(500); // 500ms warten bis gripperState erneut
+									// abgefragt wird
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Etwas Abstand von der Barrel nehmen
+		Position newPos = new Position(); // TODO Abstand zur Barrel als Position berechnen!
+		planner.addIsDoneListener(new IPlannerListener() {
+			@Override
+			public void callWhenIsDone() {
+				state = RobotState.NEEDS_NEW_GOAL;
+			}
+			@Override
+			public void callWhenAbort() {}
+			@Override
+			public void callWhenNotValid() {
+				logger.info("No valid path");
+				state = RobotState.NEEDS_NEW_GOAL;
+			}
+		});
+		planner.setGoal(newPos);
 	}
 
 	public abstract void doStep();
@@ -74,6 +202,7 @@ public abstract class PatrolRobot extends NavRobot {
 		return null;
 	}
 	
+	// TODO implementieren
 	private void checkForNewBarrels() {
 		if(barrelPositions != null) {
 			
